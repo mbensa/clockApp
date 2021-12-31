2d7299fb35b7b21f1f6332a041d9856bf2a7c99c
Mojca Bensa <mojca.bensa@gmail.com>
2021-12-31 20:22:25 +0100

    changed fetch data

--- src/App.jsx
+++ src/App.jsx
@@ -16,7 +16,7 @@ class App extends Component {
     const myTz = Intl.DateTimeFormat().resolvedOptions().timeZone;
 
     const worldTimeApiPromise = new Promise((resolve, reject) => {
-      fetch(`https://clockapp.mbensa.com/.netlify/functions/wc?tz=${myTz}`)
+      fetch(`.netlify/functions/wc?tz=${myTz}`)
         .then((data) => resolve(data.json()))
         .catch(reject);
     });
